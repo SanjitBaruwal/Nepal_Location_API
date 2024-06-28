461694e369393ed2736c289b08765e7c7a71f3b0
Sanjit Baruwal <96007399+SanjitBaruwal@users.noreply.github.com>
2024-06-29 01:19:10 +0545

    Update app.js

--- API/app.js
+++ API/app.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://nepal-location-api-client.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
